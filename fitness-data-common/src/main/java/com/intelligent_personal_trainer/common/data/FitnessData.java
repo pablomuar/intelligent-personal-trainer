932482bea8e26a13fe8c6e5e41e9811df19096ec
pablomuar <121102111+pablomuar@users.noreply.github.com>
2025-11-29 19:10:48 +0100

    Fixed Kafka Data client and server issues

--- fitness-data-common/src/main/java/com/intelligent_personal_trainer/common/data/FitnessData.java
+++ fitness-data-common/src/main/java/com/intelligent_personal_trainer/common/data/FitnessData.java
@@ -1,13 +1,17 @@
 package com.intelligent_personal_trainer.common.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FitnessData {
     private Instant timestamp;
     private String userId;
--- fitness-data-common/src/main/java/com/intelligent_personal_trainer/common/data/WorkoutData.java
+++ fitness-data-common/src/main/java/com/intelligent_personal_trainer/common/data/WorkoutData.java
@@ -1,10 +1,14 @@
 package com.intelligent_personal_trainer.common.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkoutData {
     private String workoutType;
     private int durationMinutes;
